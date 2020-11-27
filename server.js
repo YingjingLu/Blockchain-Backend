451f49d88c42ccd6ec4ad94dc3ca36fa4e3f7210
YingjingLu <yingjinglu2019us@gmail.com>
2020-11-27 01:37:40 -0500

    initial

---
+++ server.js
@@ -0,0 +1,65 @@
+const util = require('./util');
+const mkdirp = require('mkdirp');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const { get_run_name_from_zip } = require('./util');
+const app = express();
+// middle ware
+app.use(express.static('public')); //to access the files in public folder
+app.use(cors()); // it enables all cors requests
+app.use(fileUpload());
+// file upload api
+app.post('/upload', (req, res) => {
+    if (!req.files) {
+        return res.status(500).send({ msg: "file is not found" })
+    }
+    // accessing the file
+    const myFile = req.files.file;
+    const zip_file_directory = util.get_abs_path(myFile.name);
+    //  mv() method places the file inside public directory
+    myFile.mv(zip_file_directory, function (err) {
+        if (err) {
+            console.log(err)
+            return res.status(500).send({ msg: "Error occured" });
+        }
+        util.extract_zip_file(zip_file_directory, util.get_abs_extracted_folder());
+        util.delete_file(zip_file_directory);
+        // returing the response with file path and name
+        return res.status(200).send({ name: get_run_name_from_zip(myFile.name)});
+    });
+});
+
+app.get('/player_state/run_id/:run_id/round/:round', (req, res) => {
+    const run_name = req.params.run_id;
+    const round = req.params.round;
+    const file_path = util.get_player_state_file_name(run_name, round);
+    if (!util.file_exists(file_path)) {
+        return res.status(500).send({msg: "file not found" + file_path});
+    }
+    const json_body = util.read_json_file(file_path);
+    return res.status(200).send({data: json_body});
+});
+
+app.get('/message/run_id/:run_id/round/:round', (req, res) => {
+    const run_name = req.params.run_id;
+    const round = req.params.round;
+    const file_path = util.get_message_file_name(run_name, round);
+    if (!util.file_exists(file_path)) {
+        return res.status(500).send({msg: "file not found" + file_path});
+    }
+    const json_body = util.read_json_file(file_path);
+    return res.status(200).send({data: json_body});
+});
+
+app.get('/list_run', (req, res) => {
+    var files = util.get_all_run();
+    return res.status(200).send({data: files});
+});
+
+
+app.listen(4500, () => {
+    mkdirp.sync(util.get_abs_extracted_folder());
+    console.log('Server listening on port 4500');
+});
+
---
+++ util.js
@@ -0,0 +1,77 @@
+const FS = require('fs');
+const ZIP = require('adm-zip');
+
+function extract_zip_file(src_zip_path, target_path) {
+    const zip = new ZIP(src_zip_path);
+    zip.extractAllTo(target_path, true);
+}
+
+function delete_file(path) {
+    FS.unlinkSync(path);
+}
+
+function get_run_name_from_zip(zip_name) {
+    return zip_name.split('.')[0];
+}
+
+function get_abs_extracted_folder() {
+    return `${__dirname}/public/run/`;
+}
+
+function get_abs_extracted_folder_from_run_name(run_name) {
+    return `${__dirname}/public/run/${run_name}`;
+}
+
+function get_abs_path(path) {
+    return `${__dirname}/public/${path}`
+}
+
+function file_exists(path) {
+    return FS.existsSync(path);
+}
+
+function read_json_file(path) {    
+    var data = JSON.parse(FS.readFileSync(path));
+    return data;
+}
+
+function get_all_run() {
+    return FS.readdirSync(get_abs_extracted_folder());
+}
+
+function get_player_state_file_name(root_folder, round) {
+    const path = root_folder + "/player_state_trace/" + round.toString() + ".json";
+    return get_abs_path(path);
+}
+
+function get_message_file_name(root_folder, round) {
+    const path = root_folder + "/message_trace/" + round.toString() + ".json";
+    return get_abs_path(path);
+}
+
+function execute_java_backend(zip_path) {
+    var exec = require('child_process').exec, child;
+    child = exec('/usr/bin/java -jar ~/Applications/example.jar' + ' ' + zip_path,
+    function (error, stdout, stderr){
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if(error !== null){
+        console.log('exec error: ' + error);
+        }
+    });
+} 
+
+module.exports = {
+    extract_zip_file,
+    get_abs_extracted_folder,
+    get_abs_path,
+    file_exists,
+    read_json_file,
+    get_player_state_file_name,
+    get_message_file_name,
+    execute_java_backend,
+    delete_file,
+    get_run_name_from_zip,
+    get_abs_extracted_folder_from_run_name,
+    get_all_run
+};
